cf0d08b36f655e714ab639efa50c851e1e4f8175
박성호 <solser12@naver.com>
2021-04-03 05:29:08 +0900

    123

--- src/main/java/com/example/demo/temp/SocketController.java
+++ src/main/java/com/example/demo/temp/SocketController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class SocketController {
 
     // /receive를 메시지를 받을 endpoint로 설정합니다.
