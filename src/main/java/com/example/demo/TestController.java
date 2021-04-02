3bf359082bafe0799e216524785885acb283bd84
박성호 <solser12@naver.com>
2021-04-03 03:21:18 +0900

    test

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 @RestController
+@Slf4j
 public class TestController {
 
     @GetMapping("/test")
     public ResponseEntity<Void> test() {
+        log.info("test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
