a6d57466e4c97b10c222123f0b0a57b632c1e794
박성호 <solser12@naver.com>
2021-04-05 07:03:23 +0900

    kurent

--- src/main/java/com/example/demo/SocketConfig.java
+++ src/main/java/com/example/demo/SocketConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import lombok.RequiredArgsConstructor;
+import org.kurento.client.KurentoClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -10,8 +11,12 @@ import org.springframework.web.socket.server.standard.ServletServerContainerFact
 
 @Configuration
 @EnableWebSocket
-@RequiredArgsConstructor
 public class SocketConfig implements WebSocketConfigurer {
+
+    @Bean
+    public KurentoClient kurentoClient() {
+        return KurentoClient.create();
+    }
 
     @Bean
     public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
