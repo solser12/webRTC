3a68cc4460cfd1e4f643c396d667d26ce11320a8
박성호 <solser12@naver.com>
2021-04-05 07:07:37 +0900

    Test

--- src/main/java/com/example/demo/SignalingConfig.java
+++ src/main/java/com/example/demo/SignalingConfig.java
@@ -2,10 +2,14 @@ package com.example.demo;
 
 import org.kurento.client.KurentoClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
+@Configuration
+@EnableWebSocket
 public class SignalingConfig implements WebSocketConfigurer {
 
     @Bean
